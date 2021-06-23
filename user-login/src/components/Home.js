792173e53c66d2d6d5bab0dfbea4821c4036375c
Your Name <desireirankundawilliams@gmail.com>
2021-06-23 20:11:06 +0200

    created a responsive home component page

--- user-login/src/components/Home.js
+++ user-login/src/components/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
     return (
         <div className="h-screen bg-gradient-to-tr from-white to-purple-500">
             <div className="bg-purple-600 shadow-2xl h-28">
-            <h1 className="lg:text-6xl flex lg:p-4 text-gray-100 sm:text-4xl p-4 md:text-6xl md:p-4 sm:p-6">ZedOne</h1>
-            <div className="sm:flex sm:flex-row sm:justify-end md:flex md:flex-row md:justify-end lg:flex lg:flex-row lg:items-center lg:justify-end mr-6 space-x-6 -mt-14">
-            <h1 className="xs:px-2 sm:px-6 sm:py-2 md:px-4 md:py-2 text-xl bg-white lg:px-6 lg:py-2 rounded-lg hover:bg-red-400 hover:text-white cursor-pointer "><Link to="/CompanyRegistration">Register</Link></h1>
-            <h1 className="sm:px-6 sm:py-2 md:px-4 md:py-2 text-xl bg-white lg:px-6 lg:py-2 rounded-lg hover:bg-red-400 hover:text-white cursor-pointer "> <Link to="/Login">Login</Link></h1>
+            <h1 className="lg:text-6xl flex items-center lg:p-4 text-gray-100 text-2xl sm:text-3xl p-8 md:text-6xl md:p-4 sm:p-6">ZedOne</h1>
+            <div className="sm:flex sm:flex-row flex items-center justify-end sm:justify-end md:flex md:flex-row md:justify-end lg:flex lg:flex-row lg:items-center lg:justify-end mr-6 space-x-6  -mt-16">
+            <h1 className="py-2 h-10 w-20 sm:px-3 p-2 sm:px-3 sm:py-2 md:px-3 md:py-2 text-base bg-white lg:px-3 lg:py-2 rounded-lg hover:bg-red-400 hover:text-white cursor-pointer "><Link to="/CompanyRegistration">Register</Link></h1>
+            <h1 className="py-2 h-10 w-16 sm:px-3 sm:py-2 md:px-3 md:py-2 text-base bg-white lg:px-3 lg:py-2 rounded-lg hover:bg-red-400 hover:text-white cursor-pointer "> <Link to="/Login">Login</Link></h1>
             </div>
             </div>
             
