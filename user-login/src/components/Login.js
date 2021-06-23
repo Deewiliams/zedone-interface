947c1d59bd847f16bf79a70a0568390c2f139685
Your Name <desireirankundawilliams@gmail.com>
2021-06-23 15:23:42 +0200

    updated the login file

--- user-login/src/components/Login.js
+++ user-login/src/components/Login.js
@@ -5,7 +5,7 @@ import log from '../images/log.png'
 const Login = () => {
     return (
         <div className="h-screen  bg-gradient-to-tr from-white to-purple-500 flex items-center justify-center">
-            <div className="bg-white shodow-2xl p-4 w-2/2 rounded-lg">
+            <div className="bg-white shodow-2xl p-4 md:w-3/4  w-3/4 rounded-lg">
                 <img className="sm:h-72 p-4 lg:px-96" src={log} alt="login image" />
                  <form className="space-y-8 lg:flex lg:items-center lg:justify-end">
                <ul className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 grid-rows-4 gap-6 lg:w-3/4 bordr">
@@ -26,7 +26,24 @@ const Login = () => {
                     placeholder="Password"
                     type="password"
                   />
+                   <button className="bg-purple-500 text-gray-100 sm:text-2xl sm:py- sm:px-8 
+                md:py-4 md:px-10 md:text-lg lg:py-4 lg:w-2/3
+                lg:px-20 lg:text-3xl hover:bg-purple-600 
+                hover:text-white rounded-lg
+                ">
+                Login
+                </button>
+                <h1>Forgotten your Password? <span className="text-red-600 font-bold">Click here</span></h1>
+
+                <button className="bg-green-500 text-gray-100 sm:text-2xl sm:py- sm:px-8 
+                md:py-4 md:px-10 md:text-lg lg:py-4 lg:w-2/3
+                lg:px-20 lg:text-3xl hover:bg-green-600 
+                hover:text-white rounded-lg
+                ">
+                Create New Business Account
+                </button>
                </ul>
+              
            </form>
             </div>
           
