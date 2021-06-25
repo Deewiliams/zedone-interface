f515748ee869f8a485b9c007f9ed14ac9a88b52e
Your Name <desireirankundawilliams@gmail.com>
2021-06-25 14:27:50 +0200

    modified the home component on the large screen responsiveness

--- user-login/src/components/Home.js
+++ user-login/src/components/Home.js
@@ -43,32 +43,59 @@ const Home = () => {
                 <h1 className="hover:text-blue-600">Contact</h1>
             </div>
             <div>
-                <h1 className="font-bold text-sm -mt-2">We Offer The Following Services</h1>
+                <h1 className="lg:text-white font-bold text-sm -mt-2 md:text-2xl">We Offer The Following Services</h1>
             </div>
             <div>
             </div>
             {/* **************Main Landing Page*************** */}
-            <div className="space-x-8 p-2">
-                <ul className="grid grid-cols-4 gap-2 grid-flows-cols ">
-                    <li className="bg-white col-span-2  shadow-2xl rounded-lg" ><div className="h-40">
-                        <img className="mt-3" src={transaction} alt="transaction image"/>
+           
+            {/* <div className="space-x-8 p-2 lg:flex lg:flex-row lg:items-center lg:justify-around">
+                <ul className="grid grid-cols-4 gap-2 lg:grid lg:grid-cols-4 lg:gap-x-2 lg:gap-y-20 lg:max-w-lg lg:min-h-10 ">
+                    <li className="bg-white col-span-2  shadow-2xl rounded-lg" ><div className="h-56 md:h-80 lg:h-16">
+                        <img className="mt-3 lg:h-40" src={transaction} alt="transaction image"/>
                         <h1 className="text-sm mt-6">Online transactions</h1>
                         </div></li>
-                    <li className="bg-white col-span-2 shadow-2xl rounded-lg" ><div className="h-40"> 
+                    <li className="bg-white col-span-2 shadow-2xl rounded-lg" ><div className="h-56 md:h-80"> 
                          <img className="mt-3" src={subscriptions} alt="transaction image"/>
                         <h1 className="text-sm mt-2">Subscriptions</h1></div></li>
-                    <li className="bg-white col-span-2 shadow-2xl rounded-lg" ><div className="h-40">
+                    <li className="bg-white col-span-2 shadow-2xl rounded-lg" ><div className="h-56 md:h-80">
                     <img className="mt-3" src={mobile} alt="transaction image"/>
                         <h1 className="text-sm mt-2">Mobile Payments</h1>
                         </div></li>
-                    <li className="bg-white col-span-2 shadow-2xl rounded-lg" ><div className="h-40">
+                    <li className="bg-white col-span-2 shadow-2xl rounded-lg" ><div className="h-56 md:h-80">
                     <img className="mt-3" src={product} alt="transaction image"/>
                         <h1 className="text-sm mt-2">Product Payments</h1>
                         </div></li>
                     
                     
                 </ul>
+            </div> */}
+
+            <div className="p-8">
+                <ul className="lg:grid lg:grid-cols-4 lg:gap-x-2">
+                    <li className="bg-white rounded-lg shadow-lg"><div className="lg:h-96">
+                        <img className="lg:mb-24 lg:mt-2 lg:grid lg:justify-center" src={transaction} alt="transaction" />
+                        <h1 className="lg:text-2xl lg:font-bold">Online Transactions</h1></div>
+                        </li>
+
+                        <li className="bg-white rounded-lg shadow-lg"><div className="lg:h-96">
+                        <img className="lg:mb-8 lg:mt-2 lg:grid lg:justify-center" src={mobile} alt="transaction" />
+                        <h1 className="lg:text-2xl lg:font-bold">Mobile Payments</h1></div>
+                        
+                        </li>
+                        <li className="bg-white rounded-lg shadow-lg"><div className="lg:h-96">
+                        <img className="lg:mb-14 lg:mt-2 lg:grid lg:justify-center" src={subscriptions} alt="transaction" />
+                        <h1 className="lg:text-2xl lg:font-bold">Subscriptions Payments</h1></div>
+                        
+                        </li>
+                        <li className="bg-white rounded-lg shadow-lg"><div className="lg:h-96">
+                        <img className="lg:mb-6 lg:mt-2 lg:grid lg:justify-center" src={product} alt="transaction" />
+                        <h1 className="lg:text-2xl lg:font-bold">Product Payments</h1></div>
+                        </li>
+                </ul>
             </div>
+          
+           
 
 
 
