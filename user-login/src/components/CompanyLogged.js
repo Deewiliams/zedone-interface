3c475a2c22597a8106f1acc726cd897f66c6ef0b
Your Name <desireirankundawilliams@gmail.com>
2021-07-01 15:15:45 +0200

    odified the companylogged file

--- user-login/src/components/CompanyLogged.js
+++ user-login/src/components/CompanyLogged.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react'
+import { FaFacebookF, FaTwitter, } from 'react-icons/fa';
 import zedone from '../images/zedone_logo_web.png'
 import barmenu from '../images/menu.svg'
 
 
 const CompanyLogged = () => {
-    const [showMenu, setShowMenu] = useState(true)
+    const [showMenu, setShowMenu] = useState(false)
     return (
+        // header section
         <div className="h-screen bg-gradient-to-tr from-white to-purple-500">
-            <div className="flex flex-row items-center justify-between p-2 bg-purple-600 h-20 lg:h-28 shadow-2xl">
+            <div className="flex flex-row items-center justify-between md:flex-row-reverse px-6 bg-purple-600 h-20 lg:h-28 shadow-2xl">
                 <div>
-                    <img className="p-4 w-16 hover:bg-purple-100 cursor-pointer overflow-hidden" onClick={() => setShowMenu(!showMenu)} src={barmenu} />
+                    <img className="p-4 w-16 md:hidden lg:hidden hover:bg-purple-100 cursor-pointer overflow-hidden" onClick={() => setShowMenu(!showMenu)} src={barmenu} />
                 </div>
 
                 <div className="">
                     <img className="w-24 md:w-40 lg:w-60" src={zedone} />
                 </div>
-                {/* <div>
+                <div className="hidden">
                 <h1 className="text-black-100 text-lg 
                 bg-white px-4 rounded-lg font-bold 
                 hover:bg-gray-300 lg:py-2 lg:shadow-2xl
@@ -23,26 +25,41 @@ const CompanyLogged = () => {
                 ">
                 Log-Out
                 </h1>
-            </div> */}
+            </div>
             </div>
 
             {/* menu button when click displays the card below */}
+
+            <div className="md:hidden lg:hidden">
+          
             {
-                showMenu ? <div className="h-5/6 bg-white w-60">
-                                <div className="bg-purple-400 h-28">hello</div>
+                showMenu ? <div className="h-5/6 bg-white w-60 md:hidden lg:hidden">
+                                <div className="bg-purple-400 h-28">Company Name</div>
+
+
+                                <div>
+                                    <div>
+                                    <h1>Settings</h1>
+                                    <h1>Help</h1>
+                                        </div> 
+                                        </div>
 
                                 {/* social media icons */}
-                            <div className="mt-72"> 
-                                <h1>facebook</h1>
-                                <h1>instagram</h1>
-                                <h1>twitter</h1>
+                            <div className="mt-52 space-x-4  text-red-500  "> 
+                                <h1></h1>
+                                <FaFacebookF className="mr-10"/>
+                                <h1 className="-mt-5 hover:text-gray-500 cursor-pointer">Like us on Facebook</h1>
+                                <FaTwitter className="mr-10" />
+                                <h1 className="-mt-5 hover:text-gray-500 cursor-pointer">Follow us on Twitter</h1>
+                                <FaFacebookF className="mr-10"/>
+                                <h1 className="-mt-5 hover:text-gray-500 cursor-pointer ">Give us Feedback</h1>
                             </div>
 
                             {/* logging out button */}
-                                <div className="p-2 mt-8">
+                                <div className="p-2 mt-4">
                                 <h1 className="text-black-100 text-lg 
                                 bg-blue-100  py-2  rounded-lg  
-                                hover:bg-gray-300 l
+                                hover:bg-blue-300 l
                                cursor-pointer
                                 ">
                                 Log-Out
@@ -51,6 +68,7 @@ const CompanyLogged = () => {
                                 </div> 
                              : null
                                 }
+                              </div>
 
 
         </div>
