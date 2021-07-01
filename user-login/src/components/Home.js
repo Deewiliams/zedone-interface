1b83ca9a3ae268f0d025fbf10cd9ac4cf76710d1
Your Name <desireirankundawilliams@gmail.com>
2021-07-01 17:10:16 +0200

    created a navbar with a dropdow for Register here

--- user-login/src/components/Home.js
+++ user-login/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import transaction from '../images/transaction.png'
 import mobile from '../images/mobile.png'
 import subscriptions from '../images/Subscriptions.png'
@@ -8,37 +8,28 @@ import { Link } from 'react-router-dom'
 
 
 const Home = () => {
+    const [clickRegister,setClickRegister] = useState(false);
     return (
         // BUttons for Logging and Registration
-        <div className="h-screen-full bg-gradient-to-tr from-white to-purple-500">
-            <div className="bg-purple-600 shadow-2xl h-28">
-                    <h1 className="lg:text-6xl flex items-center lg:p-6 text-gray-100 text-2xl sm:text-3xl p-8 md:text-6xl md:p-4 sm:p-6">
-                    <img className="w-24 md:w-52" src={zedone} /></h1>
-
-            <div className="sm:flex sm:flex-row flex items-center justify-end sm:justify-end md:flex md:flex-row md:justify-end lg:flex lg:flex-row lg:items-center lg:justify-end mr-6 space-x-6  -mt-16">
-                    <h1 className="py-2 h-10 w-20 sm:px-3 p-2 sm:py-2 
-                    md:px-3 md:py-2 text-base bg-white lg:px-3 lg:py-2 rounded-lg 
-                   hover:bg-red-400 hover:text-white cursor-pointer">
-                    <Link to="/CompanyRegistration">
-                    Register
-                    </Link>
-                    </h1>
-
-                    <h1 className="py-2 h-10 w-16 sm:px-3 sm:py-2 md:px-3 md:py-2
-                    text-base bg-white lg:px-3 lg:py-2 rounded-lg hover:bg-red-400 
-                  hover:text-white cursor-pointer ">
-                    <Link to="/Login">
-                     Login</Link>
-                    </h1>
-
-                    <h1 className="py-2 h-10 w-16 sm:px-3 sm:py-2 md:px-3 md:py-2
-                     text-base bg-white lg:px-3 lg:py-2 rounded-lg hover:bg-red-400 
-                    hover:text-white cursor-pointer ">
-                        <Link to="/users">
-                     Users-register</Link>
-                    </h1>
-                </div>
-            </div>
+        <div className="h-screen-full lg:h-screen bg-gradient-to-tr from-white to-purple-500">
+       <div className="bg-purple-600 h-20 shadow-2xl">
+           <div className="flex flex-row justify-between p-4">
+           <div>
+            <img className="w-28" src={zedone} alt="logo" />
+           </div>
+           <div className="text-white ">
+               <h1 className="hover:text-black cursor-pointer" onClick={() => setClickRegister(!clickRegister)}>Register here</h1>
+               <h1 className="hover:text-black cursor-pointer">Login</h1>
+           </div>
+           </div>
+       </div>
+                {
+                    clickRegister?  <div className=" ml-60 pb-3 bg-white w-24 -mt-10">
+                    <h1 className="hover:bg-purple-200" ><Link to="/CompanyRegistration">Company</Link></h1>
+                    <h1 className="hover:bg-purple-200"><Link to="/">Personal</Link></h1>
+                </div> : null
+                }
+      
         {/* ****************Landing Page Navbars******************** */}
         
             <div className="flex flex-row justify-end space-x-2 p-4 text-sm text-gray-50 md:text-2xl md:space-x-5 cursor-pointer">
@@ -48,17 +39,17 @@ const Home = () => {
                 <h1 className="hover:text-blue-600">Contact</h1>
             </div>
             <div>
-                <h1 className="lg:text-white md:text-red-600 font-bold text-sm -mt-2 md:text-2xl">We Offer The Following Services</h1>
+                <h1 className="lg:text-white text-white font-bold text-sm -mt-2 md:text-2xl">We Offer The Following Services</h1>
             </div>
             <div>
             </div>
             {/* **************Main Landing Page*************** */}
         
                 <div>
-                    <ul className="grid gap-4 p-4">
+                    <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 p-4">
                         <li className="bg-white  rounded-lg">
                             <img className="p-2" src={transaction} />
-                            <div><h1 className="text-2xl">Online Transactions</h1>
+                            <div className="mt-16"><h1 className="text-2xl">Online Transactions</h1>
                             <button className="bg-purple-300 px-4 py-2 mb-4 
                             hover:bg-purple-500 hover:text-white cursor-pointer rounded-lg
                             ">
@@ -78,7 +69,7 @@ const Home = () => {
 
                             <li className="bg-white  rounded-lg">
                             <img className="p-2" src={subscriptions} />
-                            <div><h1 className="text-2xl">Subscriptions Payments</h1>
+                            <div className="mt-6"><h1 className="text-2xl">Subscriptions Payments</h1>
                             <button className="bg-purple-300 px-4 py-2 mb-4 
                             hover:bg-purple-500 hover:text-white cursor-pointer rounded-lg
                             ">
