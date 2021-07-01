c3b6d03aaebee9f2cf8d2c71bd10e5148e428f30
Your Name <desireirankundawilliams@gmail.com>
2021-07-01 15:53:14 +0200

    modified home file with new feature

--- user-login/src/components/Home.js
+++ user-login/src/components/Home.js
@@ -10,10 +10,11 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         // BUttons for Logging and Registration
-        <div className="h-screen md:min-h-full ... bg-gradient-to-tr from-white to-purple-500">
+        <div className="h-screen-full bg-gradient-to-tr from-white to-purple-500">
             <div className="bg-purple-600 shadow-2xl h-28">
                     <h1 className="lg:text-6xl flex items-center lg:p-6 text-gray-100 text-2xl sm:text-3xl p-8 md:text-6xl md:p-4 sm:p-6">
                     <img className="w-24 md:w-52" src={zedone} /></h1>
+
             <div className="sm:flex sm:flex-row flex items-center justify-end sm:justify-end md:flex md:flex-row md:justify-end lg:flex lg:flex-row lg:items-center lg:justify-end mr-6 space-x-6  -mt-16">
                     <h1 className="py-2 h-10 w-20 sm:px-3 p-2 sm:py-2 
                     md:px-3 md:py-2 text-base bg-white lg:px-3 lg:py-2 rounded-lg 
@@ -22,12 +23,14 @@ const Home = () => {
                     Register
                     </Link>
                     </h1>
+
                     <h1 className="py-2 h-10 w-16 sm:px-3 sm:py-2 md:px-3 md:py-2
                     text-base bg-white lg:px-3 lg:py-2 rounded-lg hover:bg-red-400 
                   hover:text-white cursor-pointer ">
                     <Link to="/Login">
                      Login</Link>
                     </h1>
+
                     <h1 className="py-2 h-10 w-16 sm:px-3 sm:py-2 md:px-3 md:py-2
                      text-base bg-white lg:px-3 lg:py-2 rounded-lg hover:bg-red-400 
                     hover:text-white cursor-pointer ">
@@ -51,30 +54,52 @@ const Home = () => {
             </div>
             {/* **************Main Landing Page*************** */}
         
+                <div>
+                    <ul className="grid gap-4 p-4">
+                        <li className="bg-white  rounded-lg">
+                            <img className="p-2" src={transaction} />
+                            <div><h1 className="text-2xl">Online Transactions</h1>
+                            <button className="bg-purple-300 px-4 py-2 mb-4 
+                            hover:bg-purple-500 hover:text-white cursor-pointer rounded-lg
+                            ">
+                            More Information
+                            </button>
+                            </div></li>
 
-            <div className="p-8 ">
-                <ul className="grid  gap-y-16 grid-cols-1 md:grid-cols-2 md:gap-x-4 md:gap-y-20 lg:grid lg:grid-cols-4 lg:gap-x-2">
-                    <li className="bg-white rounded-lg shadow-lg"><div className="h-72 sm:h-24 md:h-96 lg:h-96">
-                        <img className="mt-2 md:mt-2 lg:mb-24 lg:mt-2 lg:grid lg:justify-center" src={transaction} alt="transaction" />
-                        <h1 className="lg:text-2xl lg:font-bold">Online Transactions</h1></div>
-                        </li>
+                            <li className="bg-white  rounded-lg">
+                            <img className="p-2" src={mobile} />
+                            <div><h1 className="text-2xl">Mobile Payments</h1>
+                            <button className="bg-purple-300 px-4 py-2 mb-4 
+                            hover:bg-purple-500 hover:text-white cursor-pointer rounded-lg
+                            ">
+                            More Information
+                            </button>
+                            </div></li>
 
-                        <li className="bg-white rounded-lg shadow-lg"><div className="h-80 lg:h-96">
-                        <img className="mt-2 md:mt-2 lg:mb-8 lg:mt-2 lg:grid lg:justify-center" src={mobile} alt="transaction" />
-                        <h1 className="lg:text-2xl lg:font-bold">Mobile Payments</h1></div>
-                        </li>
+                            <li className="bg-white  rounded-lg">
+                            <img className="p-2" src={subscriptions} />
+                            <div><h1 className="text-2xl">Subscriptions Payments</h1>
+                            <button className="bg-purple-300 px-4 py-2 mb-4 
+                            hover:bg-purple-500 hover:text-white cursor-pointer rounded-lg
+                            ">
+                            More Information
+                            </button>
+                            </div></li>
 
-                        <li className="bg-white rounded-lg shadow-lg"><div className="h-72 lg:h-96">
-                        <img className="mt-2 md:mt-2 lg:mb-24 lg:mt-2 lg:grid lg:justify-center" src={subscriptions} alt="transaction" />
-                        <h1 className="lg:text-2xl lg:-mt-8 lg:font-bold">Subscriptions Payments</h1></div>
-                        </li>
-
-                        <li className="bg-white rounded-lg shadow-lg"><div className="h-60 lg:h-96">
-                        <img className="mt-2 md:mt-2 lg:mb-8 lg:mt-2 lg:grid lg:justify-center" src={product} alt="transaction" />
-                        <h1 className="lg:text-2xl lg:-mt-1 lg:font-bold">Product Payments</h1></div>
-                        </li>
-                </ul>
-            </div>
+                            <li className="bg-white  rounded-lg">
+                            <img className="p-2" src={product} />
+                            <div><h1 className="text-2xl">Product Payments</h1>
+                            <button className="bg-purple-300 px-4 py-2 mb-4 
+                            hover:bg-purple-500 hover:text-white cursor-pointer rounded-lg
+                            ">
+                            More Information
+                            </button>
+                            </div></li>
+                        
+                        
+                        
+                    </ul>
+                </div>
  </div>
 
     )
